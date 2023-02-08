d17067731c44f8197d71521b4ae8e588c67c077d
Dima Syzonenko <sizonenko.d18@gmail.com>
2023-02-08 16:53:32 +0200

    Updates

--- script.js
+++ script.js
@@ -1,1 +1,113 @@
-!function(){"use strict";window.addEventListener("DOMContentLoaded",(()=>{((e,t,n)=>{const i=document.querySelector(e),a=document.querySelectorAll(t),s=document.querySelectorAll(n);let d=1,c=!1;function r(e){e>a.length&&(d=1),e<1&&(d=a.length),a.forEach((e=>e.classList.remove("active-slide"))),a[d-1].classList.add("active-slide"),s.forEach((e=>e.classList.remove("active"))),s[d-1].classList.add("active")}function l(){c=setInterval((function(){r(d+=1)}),7e3)}s.forEach((e=>{e.addEventListener("click",(e=>{const t=e.target.getAttribute("data-slide-to");d=t,r(d)}))})),l(),i.parentNode.addEventListener("mouseenter",(()=>{clearInterval(c)})),i.parentNode.addEventListener("mouseleave",(()=>{l()}))})(".main__slider",".main__slider-slide",".main__slider-indicators li")}))}();
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/modules/slider.js":
+/*!**********************************!*\
+  !*** ./src/js/modules/slider.js ***!
+  \**********************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+const slider = (slidesFieldSelector, slidesSelector, dotsSelector) => {
+  const slidesField = document.querySelector(slidesFieldSelector),
+    slides = document.querySelectorAll(slidesSelector),
+    dots = document.querySelectorAll(dotsSelector);
+  let slideIndex = 1;
+  let paused = false;
+  function showSlides(n) {
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+    slides.forEach(slide => slide.classList.remove('active-slide'));
+    slides[slideIndex - 1].classList.add('active-slide');
+    dots.forEach(dot => dot.classList.remove('active'));
+    dots[slideIndex - 1].classList.add('active');
+  }
+  function nextSlide(n) {
+    showSlides(slideIndex += n);
+  }
+  dots.forEach(el => {
+    el.addEventListener('click', event => {
+      const slideTo = event.target.getAttribute('data-slide-to');
+      slideIndex = slideTo;
+      showSlides(slideIndex);
+    });
+  });
+  function activateAnimation() {
+    paused = setInterval(function () {
+      nextSlide(1);
+    }, 7000);
+  }
+  activateAnimation();
+  slidesField.parentNode.addEventListener('mouseenter', () => {
+    clearInterval(paused);
+  });
+  slidesField.parentNode.addEventListener('mouseleave', () => {
+    activateAnimation();
+  });
+};
+/* harmony default export */ __webpack_exports__["default"] = (slider);
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	!function() {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = function(exports) {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+!function() {
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _modules_slider__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/slider */ "./src/js/modules/slider.js");
+
+window.addEventListener('DOMContentLoaded', () => {
+  'use strict';
+
+  (0,_modules_slider__WEBPACK_IMPORTED_MODULE_0__["default"])('.main__slider', '.main__slider-slide', '.main__slider-indicators li');
+});
+}();
+/******/ })()
+;
+//# sourceMappingURL=script.js.map
